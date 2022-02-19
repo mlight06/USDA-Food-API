f707c2eff5ca3907a627a7124bb470ca30e78039
Michael Light <mklight06@gmail.com>
2022-02-19 09:44:14 -0700

    shifted FoodList to child of Search, working on getting query param to return

--- my-app/src/FoodList.jsx
+++ my-app/src/FoodList.jsx
@@ -2,18 +2,21 @@ import React, {useEffect, useState} from "react";
 import axios from 'axios';
 import { API_KEY } from "./config";
 
-export default function FoodList() {
-  const [foodList, setFoodList] = useState([]);
+export default function FoodList(props) {
+  const {searchItem} = props;
+  const {foodList} = props;
+  // const [foodList, setFoodList] = useState([]);
   const [additionalDetails, setAdditionalDetails] = useState(false);
+  console.log('searchITEM', searchItem)
 
-  useEffect(() => {
-    axios.get(`https://api.nal.usda.gov/fdc/v1/food/470794?nutrients=203&nutrients=204&nutrients=205&nutrients=208&api_key=${API_KEY}`)
-    .then(response => {
-      setFoodList(response.data);
-      console.log('response', response.data)
-    })
-    .catch(err => console.error(err))
-  },[])
+  // useEffect(() => {
+  //   axios.get(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${searchItem}&nutrients=203&nutrients=204&nutrients=205&nutrients=208&api_key=${API_KEY}`)
+  //   .then(response => {
+  //     setFoodList(response.data);
+  //     console.log('response', response.data)
+  //   })
+  //   .catch(err => console.error(err))
+  // },[])
 
   function additionalDetToTrue() {
     setAdditionalDetails(true);
--- my-app/src/Search.jsx
+++ my-app/src/Search.jsx
@@ -1,16 +1,27 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import FoodList from "./FoodList";
+import { API_KEY } from "./config";
 
 export default function Search() {
   const [searchItem, setSearchItem] = useState('');
+  const [foodList, setFoodList] = useState([]);
   console.log('searchItem', searchItem)
 
-
+function search() {
+  console.log('made it to search')
+  axios.get(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${searchItem}&api_key=${API_KEY}`)
+  .then(response => {
+    setFoodList(response.data);
+    console.log('response', response.data)
+  })
+  .catch(err => console.error(err))
+}
   return (
     <div>
-        <form action="/" method="get">
-        <label htmlFor="header-search">
-            <span className="visually-hidden">Search food items</span>
+        <form >
+        <label >
+            <span>Search food items</span>
         </label>
         <input
             type="text"
@@ -19,9 +30,9 @@ export default function Search() {
             name="s"
             onInput={e => setSearchItem(e.target.value)}
         />
-        <button type="submit">Search</button>
+        <button type="submit" onClick={search}>Search</button>
     </form>
-
+    <FoodList searchItem={searchItem} foodList={foodList}/>
     </div>
   )
 }
