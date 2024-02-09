361cfe95cde6b1d4eefc587607b2e5c3d26f5101
Michael Light <mklight06@gmail.com>
2024-02-09 09:43:41 -0700

    successfully displaying 5 results from api

--- my-app/client/src/FoodList.jsx
+++ my-app/client/src/FoodList.jsx
@@ -12,7 +12,7 @@ export default function FoodList(props) {
 
   //Implementation of Pagination
   const [currentPage, setCurrentPage] = useState(1);
-  const resultsPerPage = useState(5);
+  const [resultsPerPage] = useState(5);
   // ending index to slice results array
   const indexOfLastPage = currentPage * resultsPerPage;
   // starting index, based on ending results
@@ -20,10 +20,13 @@ export default function FoodList(props) {
 
   const displayedResults = foodList.slice(indexOfFirstPage, indexOfLastPage)
 
+  console.log('displayedResults', displayedResults, 'first', indexOfFirstPage, 'last', indexOfLastPage, 'curr', currentPage, resultsPerPage)
+
   useEffect(() => {
     axios.get(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${searchItem}&pageSize=10&api_key=${API_KEY}`)
     .then(response => {
       setFoodList(response.data.foods);
+      console.log('displayedResults', displayedResults)
     })
     .catch(err => console.error(err))
   },[searchItem])
@@ -31,8 +34,8 @@ export default function FoodList(props) {
   return (
     <Container sx={{maxWidth: 700}}>
       <Cart cartList={cartList} setCartList={setCartList}/>
-      <Container sx={{maxWidth: 700}}>
-        {foodList.length > 0 ? foodList.map((foodItem) => (
+      <Container sx={{maxWidth: 700}} className="foodList-container">
+        {foodList.length > 0 ? displayedResults.map((foodItem) => (
 
                   <FoodItem foodItem={foodItem} setCartList={setCartList}/>
 
