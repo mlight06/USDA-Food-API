c0b306478e18eaa57e7cd10c4b1cf273521ea4de
Michael Light <mklight06@gmail.com>
2024-02-09 09:23:07 -0700

    implementing pagination for foodlist results

--- my-app/client/src/FoodList.jsx
+++ my-app/client/src/FoodList.jsx
@@ -10,6 +10,16 @@ export default function FoodList(props) {
   const [foodList, setFoodList] = useState([]);
   const [cartList, setCartList] = useState([]);
 
+  //Implementation of Pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const resultsPerPage = useState(5);
+  // ending index to slice results array
+  const indexOfLastPage = currentPage * resultsPerPage;
+  // starting index, based on ending results
+  const indexOfFirstPage = indexOfLastPage - resultsPerPage;
+
+  const displayedResults = foodList.slice(indexOfFirstPage, indexOfLastPage)
+
   useEffect(() => {
     axios.get(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${searchItem}&pageSize=10&api_key=${API_KEY}`)
     .then(response => {
